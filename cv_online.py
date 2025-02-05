99c6901af70f8b0de054235e939db0dd2df27aec
Papa SEYE <papaseye296@gmail.com>
2025-02-05 16:19:15 +0000

    Modifier résumé

--- cv_online.py
+++ cv_online.py
@@ -35,7 +35,7 @@ st.markdown("🏠 Rue 27X18, Médina, Dakar, Sénégal")
 # Section PRINCIPALES COMPETENCES
 
 st.header("RESUME")
-st.markdown("Data Engineer / Data Scientist avec une expertise en Big Data, ingénierie des données et analyse décisionnelle. Expérience dans la conception et l’optimisation de data lakes, ETL et entrepôts de données en environnement on-premise et cloud. Maîtrise des technologies Hadoop, Spark, Kafka, SQL, NoSQL, Python et Power BI pour le traitement et la visualisation des données. Passionné par l’innovation et la mise en place de solutions analytiques performantes.")
+st.markdown("Data Engineer / Data Scientist, spécialisé en Big Data, ingénierie des données et intelligence artificielle. Expérience dans la conception et l’optimisation de data lakes, ETL et entrepôts de données en environnement on-premise et cloud. Maîtrise des technologies Hadoop, Spark, Kafka, SQL, NoSQL, Python et Power BI pour le traitement et la visualisation des données. Compétences en LLM, LangChain et Streamlit pour le développement d’applications basées sur l’IA générative. Passionné par l’innovation et la mise en place de solutions analytiques et intelligentes.")
 st.header("PRINCIPALES COMPETENCES")
 
 # Sous-section COMPETENCES
